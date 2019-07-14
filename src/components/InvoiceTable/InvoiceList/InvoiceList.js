5a2a9e1562179c562826746f94025b6ba8f06a03
dobernike <dobernike@mail.ru>
2019-07-14 23:30:45 +0300

    Добавляет список к таблице

---
+++ src/components/InvoiceTable/InvoiceList/InvoiceList.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import InvoiceItem from "./InvoiceItem/InvoiceItem";
+
+export default function InvoiceList() {
+  const [name, setName] = useState("");
+  const [count, setCount] = useState(1);
+  const [price, setPrice] = useState("");
+  const [rows, setRows] = useState([]);
+  const [index, setIndex] = useState(1);
+
+  function addRow() {
+    setRows([
+      ...rows,
+      <InvoiceItem
+        key={Math.random() + name}
+        index={index}
+        name={name}
+        count={count}
+        price={price}
+      />
+    ]);
+
+    setIndex(index + 1);
+    setName("");
+    setCount(1);
+    setPrice("");
+  }
+
+  function changeStateHandler(e, state) {
+    state(e.target.value);
+  }
+
+  return (
+    <>
+      {rows}
+      <tr className="printHide">
+        <td colSpan="5" style={{ textAlign: "left" }}>
+          <input
+            type="text"
+            style={{ width: "300px", marginRight: "10px" }}
+            placeholder="название"
+            value={name}
+            onChange={e => changeStateHandler(e, setName)}
+          />
+          <input
+            type="number"
+            style={{ width: "30px", marginRight: "10px" }}
+            placeholder="1"
+            value={count}
+            onChange={e => changeStateHandler(e, setCount)}
+          />
+          <input
+            type="number"
+            style={{ width: "60px" }}
+            placeholder="₽"
+            value={price}
+            onChange={e => changeStateHandler(e, setPrice)}
+          />
+        </td>
+        <td>
+          <button onClick={addRow}>add</button>
+        </td>
+      </tr>
+    </>
+  );
+}
