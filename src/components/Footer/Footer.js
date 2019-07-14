4077067b4d5c41bbb9ce024507ede9d7fb853d61
dobernike <dobernike@mail.ru>
2019-07-14 23:30:13 +0300

    Добавляет футер

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <table id="subscripts">
+      <tbody>
+        <tr>
+          <th>Отпустил</th>
+          <td>&nbsp;</td>
+          <th>Получил</th>
+          <td>&nbsp;</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
