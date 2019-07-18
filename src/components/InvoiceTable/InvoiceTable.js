885c8288592fdd59a68af69c8341e7ba50fafaf0
dobernike <dobernike@mail.ru>
2019-07-18 14:13:20 +0300

    Добавляет логику отрисовки доставки и общей total

--- src/components/InvoiceTable/InvoiceTable.js
+++ src/components/InvoiceTable/InvoiceTable.js
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
+import './InvoiceTable.css'
 import InvoiceList from "./InvoiceList/InvoiceList";
+import numberToString from './numberToString';
 
 export default function InvoiceTable() {
   const [total, setTotal] = useState(0);
   const [index, setIndex] = useState(0);
+  const [delivery, setDelivery] = useState(true);
+
+  const TO_DELIVERY_FREE = 3000;
+  const DELIVERY_PRICE = 300;
 
   function updateTotal(price) {
+    if (price < TO_DELIVERY_FREE) {
+      setDelivery(true);
+      price += DELIVERY_PRICE
+    } else {
+      setDelivery(false);
+    }
+
     setTotal(price);
   }
 
   function updateIndex(idx) {
     setIndex(idx);
+  }
+
+  function onDeleteDelivery() {
+    setDelivery(false);
+    setTotal(prevTotal => prevTotal - DELIVERY_PRICE);
   }
 
   return (
@@ -27,17 +45,24 @@ export default function InvoiceTable() {
           </tr>
         </thead>
         <tfoot>
+          {delivery ?
+            <tr className="total">
+              <td colSpan="5">Доставка:</td>
+              <td>{DELIVERY_PRICE}</td>
+              <td className="printHide delete delete--delivery" onClick={onDeleteDelivery}>x</td>
+            </tr>
+            : null}
           <tr className="total">
             <td colSpan="5">Итого:</td>
-            <td>{total}</td>
+            <td className='price'>{total}</td>
           </tr>
         </tfoot>
         <tbody>
           <InvoiceList updateTotal={updateTotal} updateIndex={updateIndex} />
         </tbody>
       </table>
-      <p>Всего наименований {index} на сумму {total} руб.</p>
-      <p id="sum-names">Одна тысяча двести рублей ноль копеек</p>
+      <p>Всего наименований {index} на сумму <span className="price">{total}</span> руб.</p>
+      <p id="sum-names">{numberToString(total)}</p>
     </>
   );
 }
