eba98605f40d01b9c19ec324241d9505f16fa9cd
dobernike <dobernike@mail.ru>
2019-07-21 12:48:12 +0300

    Добавляет кнопку печати страницы и автопечать

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,23 @@
 import React from "react";
+import './Footer.css';
 
 export default function Footer() {
   return (
-    <table id="subscripts">
-      <tbody>
-        <tr>
-          <th>Отпустил</th>
-          <td>&nbsp;</td>
-          <th>Получил</th>
-          <td>&nbsp;</td>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      <table id="subscripts">
+        <tbody>
+          <tr>
+            <th>Отпустил</th>
+            <td>&nbsp;</td>
+            <th>Получил</th>
+            <td>&nbsp;</td>
+          </tr>
+        </tbody>
+      </table>
+      <button className="printBtn printHide" onClick={() => window.printpage()}>
+        Печать
+      </button>
+    </>
   );
 }
 
