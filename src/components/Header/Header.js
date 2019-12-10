46e648dc05e79c96d414fb62c108451eacaa1578
Paul <dobernike@mail.ru>
2019-12-11 00:25:19 +0300

    Убирает лишнее

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,7 +13,7 @@ export default function Header() {
 
   useEffect(() => {
     document.title = `${date} накладная № ${invoiceNumber}`;
-  }, [date, invoiceNumber])
+  }, [date, invoiceNumber]);
 
   function onChangeDate(e) {
     setDate(e.target.value);
@@ -54,39 +54,39 @@ export default function Header() {
             </td>
             <td id="pay_till">
               <svg
-                xmlns='http://www.w3.org/2000/svg'
-                x='0'
-                y='0'
-                version='1.1'
-                viewBox='0 0 178 70'
+                xmlns="http://www.w3.org/2000/svg"
+                x="0"
+                y="0"
+                version="1.1"
+                viewBox="0 0 178 70"
                 width="178px"
                 height="70px"
                 alt="штрихкод накладной"
               >
-                <path fill='#fff' d='M0 0H178V70H0z' />
-                <g transform='translate(10 10)'>
-                  <path d='M0 0H4V50H0z' />
-                  <path d='M6 0H8V50H6z' />
-                  <path d='M12 0H18V50H12z' />
-                  <path d='M22 0H30V50H22z' />
-                  <path d='M32 0H34V50H32z' />
-                  <path d='M36 0H38V50H36z' />
-                  <path d='M44 0H46V50H44z' />
-                  <path d='M52 0H54V50H52z' />
-                  <path d='M60 0H64V50H60z' />
-                  <path d='M66 0H72V50H66z' />
-                  <path d='M74 0H76V50H74z' />
-                  <path d='M78 0H86V50H78z' />
-                  <path d='M88 0H94V50H88z' />
-                  <path d='M96 0H98V50H96z' />
-                  <path d='M102 0H106V50H102z' />
-                  <path d='M110 0H114V50H110z' />
-                  <path d='M116 0H122V50H116z' />
-                  <path d='M124 0H130V50H124z' />
-                  <path d='M132 0H136V50H132z' />
-                  <path d='M142 0H148V50H142z' />
-                  <path d='M150 0H152V50H150z' />
-                  <path d='M154 0H158V50H154z' />
+                <path fill="#fff" d="M0 0H178V70H0z" />
+                <g transform="translate(10 10)">
+                  <path d="M0 0H4V50H0z" />
+                  <path d="M6 0H8V50H6z" />
+                  <path d="M12 0H18V50H12z" />
+                  <path d="M22 0H30V50H22z" />
+                  <path d="M32 0H34V50H32z" />
+                  <path d="M36 0H38V50H36z" />
+                  <path d="M44 0H46V50H44z" />
+                  <path d="M52 0H54V50H52z" />
+                  <path d="M60 0H64V50H60z" />
+                  <path d="M66 0H72V50H66z" />
+                  <path d="M74 0H76V50H74z" />
+                  <path d="M78 0H86V50H78z" />
+                  <path d="M88 0H94V50H88z" />
+                  <path d="M96 0H98V50H96z" />
+                  <path d="M102 0H106V50H102z" />
+                  <path d="M110 0H114V50H110z" />
+                  <path d="M116 0H122V50H116z" />
+                  <path d="M124 0H130V50H124z" />
+                  <path d="M132 0H136V50H132z" />
+                  <path d="M142 0H148V50H142z" />
+                  <path d="M150 0H152V50H150z" />
+                  <path d="M154 0H158V50H154z" />
                 </g>
               </svg>
             </td>
@@ -99,13 +99,8 @@ export default function Header() {
             <td>
               <ul id="receiver">
                 <b>
-                  <li>
-                    Адрес поставки: Стрельна, ул. Нижняя Колония, д. 49Б
-                    +79626803377
-                  </li>
-                  <li>
-                    Поставщик: Индивидуальный Предприниматель Федюшин П.Д.
-                  </li>
+                  <li>Адрес поставки: Адрес</li>
+                  <li>Поставщик: Имя</li>
                 </b>
               </ul>
             </td>
